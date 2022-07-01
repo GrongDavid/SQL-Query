37079623c8143202c5e9a5c8ab5d3d76ae043c14
david <davidgrong778@gmail.com>
2022-07-01 13:51:37 -0500

    finished queries

---
+++ queries_products.sql
@@ -0,0 +1,16 @@
+-- Comments in SQL Start with dash-dash --
+
+INSERT INTO products (Name, Price, can_be_returned) VALUES ('chair', 44.00, false);
+INSERT INTO products (Name, Price, can_be_returned) VALUES ('stool', 25.99, true);
+INSERT INTO products (Name, Price, can_be_returned) VALUES ('table', 124.00, false);
+SELECT * FROM products;
+SELECT name FROM products;
+SELECT name, price FROM products;
+INSERT INTO products (Name, Price, can_be_returned) VALUES ('couch', 599.99, false);
+SELECT * FROM products WHERE can_be_returned = true;
+SELECT * FROM products WHERE price < 44.00;
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
+UPDATE products SET price = price - 20;
+DELETE FROM products WHERE price < 25;
+UPDATE products SET price = price + 20;
+UPDATE products SET can_be_returned = true;
